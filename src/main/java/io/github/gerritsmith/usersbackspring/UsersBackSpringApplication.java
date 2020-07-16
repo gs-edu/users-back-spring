07aaf36c10f4e87e8b7c762af4600b08577493c0
gerritsmith <63374003+gerritsmith@users.noreply.github.com>
2020-07-16 02:53:07 -0500

    initial commit

---
+++ src/main/java/io/github/gerritsmith/usersbackspring/UsersBackSpringApplication.java
@@ -0,0 +1,13 @@
+package io.github.gerritsmith.usersbackspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersBackSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersBackSpringApplication.class, args);
+	}
+
+}
